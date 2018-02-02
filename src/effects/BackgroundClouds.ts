0132f7ac7c830a9661ffb889bbde5a72a384c20c
Hoffa <robin.hofstrom@gmail.com>
2018-02-02 11:31:13 +0100

    LA till kommentarer

--- src/effects/BackgroundClouds.ts
+++ src/effects/BackgroundClouds.ts
@@ -21,11 +21,11 @@ export class BackgroundClouds {
     this._clouds = [];
 
     for( var i=0; i <  this._numberOfClouds; i++ ) {
-        this._clouds[i] = this.game.add.sprite( 300,300,"BGCloud" + i );
-        this._clouds[i].position.x += ( i * 400);
-        this._clouds[i].position.y += Math.random() * 100;
-        this._clouds[i].scale = new Phaser.Point( 0.5, 0.5 );
-        this._clouds[i].velocity = 1 + Math.random()*3;
+        this._clouds[i] = this.game.add.sprite( 300,300,"BGCloud" + i ); // X, Y, Textur ID
+        this._clouds[i].position.x += ( i * 400); // Avgör startposition Sidled
+        this._clouds[i].position.y += Math.random() * 100; // Avgör start position Höjdled
+        this._clouds[i].scale = new Phaser.Point( 0.5, 0.5 ); // Styr storleken, dvs Skalan
+        this._clouds[i].velocity = 1 + Math.random()*3; // Styr hastigheten på molnen
     }
     console.log("Clouds:", this._clouds);
   }
@@ -35,10 +35,10 @@ export class BackgroundClouds {
 
     for( var i=0; i < this._clouds.length; i++ ) {
 
-      let singleCloud = this._clouds[i];
-      singleCloud.position.x += singleCloud.velocity;
+      let singleCloud = this._clouds[i]; // Ta en instans av ett moln i loopen
+      singleCloud.position.x += singleCloud.velocity; // LÄgg till Hastigheten på molnets X position för att flytta det i sidled.
 
-      if( singleCloud.position.x > 1600 + offScreenOffset ) {
+      if( singleCloud.position.x > 1600 + offScreenOffset ) { // När molnet är utanför så flyttar vi det till andra sidan, utanför skärmen
         singleCloud.position.x = -offScreenOffset;
       }
     }
