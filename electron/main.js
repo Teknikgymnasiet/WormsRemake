8bf801e8e9a00d657491e417f96ca7faf5f935e0
sorker3 <sorker4@hotmail.com>
2018-02-16 10:45:18 +0100

    Added package 'electron' and made a folder for it. Made a index.html specificly for electron. Begun making it's main.js

---
+++ electron/main.js
@@ -0,0 +1,34 @@
+const electron = require('electron');
+const app = electron.app;
+// create native browser window.
+const BrowserWindow = electron.BrowserWindow
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 768, height: 432});
+  mainWindow.loadURL(`file://${__dirname}/electron/index.html`);
+
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  });
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed on OSX and Linux
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+// Safety
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+});
