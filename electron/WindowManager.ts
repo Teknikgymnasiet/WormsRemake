35df51d71881ceec1ceed231ede6aab27b45746a
sorker3 <sorker4@hotmail.com>
2018-02-21 19:27:40 +0100

    Added tslint. Fixed syntax in WindowManager

--- electron/WindowManager.ts
+++ electron/WindowManager.ts
@@ -1,24 +1,22 @@
 
-
 export class WindowManager {
     // Our Electron instance
-    private mainWindow;
-    // Referance Boundle.js window inside our electronwindow
-    public gameWindow;
+    public mainWindow;
 
-    public windowTitle:string = "PhaserJS Worms Remake";
-    public gameIcon = '../assets/gameIcon.png';
-    
-    protected webPreferences = {
+    public windowTitle: string = "PhaserJS Worms Remake";
+    public gameIcon = "../assets/gameIcon.png";
+
+    public webPreferences = {
         devTools: true,
         nodeIntegration: true,
         // Read about multithreading, left false as default
         nodeIntegrationInWorker: false,
+        plugins: false,
         zoomFactor: 1.0,
-        plugins: false
     };
+
     public WindowOptions = {
-        width: 600, 
+        width: 600,
         height: 900,
         title: this.windowTitle,
         icon: this.gameIcon,
@@ -29,20 +27,19 @@ export class WindowManager {
         webPreferences: this.webPreferences,
     };
 
-
-    public toggleFullscreen():void {
-        this.mainWindow.setFullscreen( !this.mainWindow.isFullscreen() );
+    public toggleFullscreen(): void {
+        this.mainWindow.setFullscreen(!this.mainWindow.isFullscreen());
     }
 
-    public ElectronIsRunning():boolean {
-        var userAgent = navigator.userAgent.toLowerCase();
-        if (userAgent.indexOf(' electron/') > -1) {
-           return true;
+    public ElectronIsRunning(): boolean {
+        const userAgent = navigator.userAgent.toLowerCase();
+        if (userAgent.indexOf(" electron/") > -1) {
+            return true;
         }
     }
 
     // Make keybind?
-    public openDevTools():void {
+    public openDevTools(): void {
         this.mainWindow.webContents.openDevTools();
     }
 }
