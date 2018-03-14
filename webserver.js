9e256e0005314f60530cb93d62a010ceb04719e8
sorker3 <sorker4@hotmail.com>
2018-03-14 23:36:49 +0100

    Added /elec for loading electron/index.html

--- webserver.js
+++ webserver.js
@@ -8,4 +8,10 @@ console.log("Starting Web Server on port " + port);
 
 
 app.use(serveStatic(__dirname, {'default': ['index.html']}));
+
+var serveElec = serveStatic(__dirname, {'index': ['electron/index.html']});
+app.use("/elec", function(req, res){
+    serveElec(req, res);
+}.bind(this));
+
 app.listen(port);
